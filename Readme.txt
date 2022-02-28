e5b151b896ff643df9a824a8dad1e542a09faa4e
Yajur21 <61120383+Yajur21@users.noreply.github.com>
2022-02-28 09:24:08 -0500

    Added readme, and added arguments into main.py

---
+++ Readme.txt
@@ -0,0 +1,18 @@
+# Step 1
+
+To generate AutoRegex Files: in this directory run:
+    python main.py --rubric_dir <dirofRubricFile> --topScorer_dir <dirOfTopScorerFile>
+        --output_file <nameOfOutputFileYouWant>
+
+# Step 2
+
+After generated AutoRegex File is made: cd LT-Autograder
+
+# Step 3
+
+Tandalla's code already has a well documented read me, please look at: Instructions_Linux.txt
+    or Instructions_Windows.txt in this directory and follow the steps
+
+    *Note: Tandalla's code needs to be run in python 2 not python 3, I made a separate conda environment
+       for this
+
--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 import csv
+import sys
+import argparse
 
 from extractPhrases import ExtractPhrase
 from util import *
@@ -13,19 +15,32 @@ from EquivalenceClass import GenerateEquivalenceClass
 #nltk.download('wordnet')
 from nltk.corpus import wordnet
 
+print('Argument List:', sys.argv)
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rubric_dir', type=str, default='Data/Data Set #1--ReadMeFirst.txt',
+                        help='data_directory')
+    parser.add_argument('--topScorer_dir', type=str,
+                        default='Data/topScorer1.txt',
+                        help='data_directory')
+    parser.add_argument('--output_file', type=str, default="output1.txt",
+                        help='LSTM hidden dimensions')
+    args = parser.parse_args()
+    print("args: ", args)
+
     # Initialize word net library, pos tagger, parser for graph generation
 
     # Step 1: Get Rubric Text and Top Scorer Text
     print("\nSTEP 1: Get Rubric Text and Top Scorer Text\n")
-    with open('Data/Data Set #9--ReadMeFirst.txt', newline='', encoding="latin-1") as rubricFile:
+    with open(args.rubric_dir, newline='', encoding="latin-1") as rubricFile:
     #with open('Data/rubric-data-temp.csv', newline='', encoding="latin-1") as rubricFile:
         #rubricReader = csv.reader(rubricFile)
         rubricSegments = [row for row in rubricFile]
 
-    print(f'rubricSegments: {rubricSegments}')
+    #print(f'rubricSegments: {rubricSegments}')
 
-    with open('Data/topScorer9.txt', newline='') as topScorerFile:
+    with open(args.topScorer_dir, newline='') as topScorerFile:
     #with open('Data/rubric-data-temp.csv', newline='') as topScorerFile:
         topScorerReader = csv.reader(topScorerFile)
         topScorerSegments = [row[0] for row in topScorerReader]
@@ -37,41 +52,41 @@ def main():
     phraseExtractor = ExtractPhrase(rubricSegments)
     rubricPhrases = phraseExtractor.extractPhrasesFromText()
 
-    print("Rubric Segments: ", rubricSegments)
-    print("Rubric Phrases: ", rubricPhrases)
+    #print("Rubric Segments: ", rubricSegments)
+    #print("Rubric Phrases: ", rubricPhrases)
 
     # Step 3: Eliminate Stop-Words
-    print("\nSTEP 3: Eliminate Stop-Words\n")
+    #print("\nSTEP 3: Eliminate Stop-Words\n")
     rubricSegments = elimStopWords(rubricSegments)
     topScorerSegments = elimStopWords(topScorerSegments)
     rubricPhrases = replaceStopWords(rubricPhrases)
 
-    print("Rubric Segments: ", rubricSegments)
-    print("Rubric Phrases: ", rubricPhrases)
+    #print("Rubric Segments: ", rubricSegments)
+    #print("Rubric Phrases: ", rubricPhrases)
 
     # Step 4: Tokenize Rubric and Top-Scorer Text
-    print("\nSTEP 4: Tokenize Rubric and Top-Scorer Text\n")
+    #print("\nSTEP 4: Tokenize Rubric and Top-Scorer Text\n")
     rubricTok = tokenizeText(rubricSegments)
     topScorerTok = tokenizeText(topScorerSegments)
 
     # Step 5: Select Most Frequent Words Among the Top-Scorer Text and Prompt Texts' Token
-    print("\nSTEP 5: Select Most Frequent Words Among the Top-Scorer Text and Prompt Texts' Token\n")
+    #print("\nSTEP 5: Select Most Frequent Words Among the Top-Scorer Text and Prompt Texts' Token\n")
     #frquencyMap = frequentToken(topScorerTok)
     topScoringTokens = frequentToken(topScorerTok)
 
     # Step 6: Identify equivalence classes for the tokens in the rubric text
-    print("\nSTEP 6: Identify equivalence classes for the tokens in the rubric text\n")
+    #print("\nSTEP 6: Identify equivalence classes for the tokens in the rubric text\n")
     equivalenceClassGen = GenerateEquivalenceClass()
     finalListOfTokenClasses = []
     for tokens in rubricTok:
         finalListOfTokenClasses = equivalenceClassGen.identifyClassOfWords(tokens, topScoringTokens, finalListOfTokenClasses)
-    print("equivalence class generated")
+    #print("equivalence class generated")
 
     #Step 7: Write out results and convert into Perl regex format
-    print("\nSTEP 7 (FINAL): Write out results and convert into Perl regex format")
-    print("finalListofTokenClasses:", finalListOfTokenClasses)
+    #print("\nSTEP 7 (FINAL): Write out results and convert into Perl regex format")
+    #print("finalListofTokenClasses:", finalListOfTokenClasses)
     finalListOfTokenClasses = regExConverter(finalListOfTokenClasses)
-    with open("output.txt", 'w') as csvfile:
+    with open(args.output_file, 'w') as csvfile:
         #csvwriter = csv.writer(csvfile)
         for phrase in finalListOfTokenClasses:
             csvfile.write(phrase + "\n")
